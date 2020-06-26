185e5287f1ced36a3e3f57707bf0a097c977bad4
David Orozco <dvdizcky@gmail.com>
2020-06-26 17:17:51 -0500

    Refactoring: Comments added also packaged models started

--- models/base_model.py
+++ models/base_model.py
@@ -1,29 +1,35 @@
 #!/usr/bin/python3
-
-
+"""BaseModel for Airbnb Project"""
 import uuid
 from datetime import datetime
 
 
 class BaseModel():
-    """class model """
+    """Base class for classes"""
 
     def __init__(self):
-        """comentario"""
-        self.id = str(uuid.uuid4())
+        """Constructor"""
+        uuid_gen = uuid.uuid4()
+
+        self.id = str(uuid_gen)
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
     def __str__(self):
-        """comentario"""
-        return("[{}] ({}) {}".format(BaseModel.__name__, self.id, self.__dict__))
+        """Returns string representation of an instance"""
+        return("[{}] ({}) {}"
+               .format(BaseModel.__name__, self.id, self.__dict__))
 
     def save(self):
+        """Save an instance and set the updated time"""
         self.updated_at = datetime.now()
 
     def to_dict(self):
+        """Returns the attributes of the instance as a dict"""
         var = self.__dict__.copy()
-        var["__class__"] = BaseModel.__name__
-        var["created_at"] = self.created_at.isoformat()
-        var["updated_at"] = self.updated_at.isoformat()
+
+        var['__class__'] = BaseModel.__name__
+        var['created_at'] = self.created_at.isoformat()
+        var['updated_at'] = self.updated_at.isoformat()
+
         return var
