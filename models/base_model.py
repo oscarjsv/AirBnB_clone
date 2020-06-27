636291b9f1ffe1c38176c7fdde262fd71844a619
David Orozco <dvdizcky@gmail.com>
2020-06-27 16:21:49 -0500

    Class name fixed

--- models/base_model.py
+++ models/base_model.py
@@ -5,25 +5,16 @@ from datetime import datetime
 
 time = "%Y-%m-%dT%H:%M:%S.%f"
 
+
 class BaseModel():
     """Base class for classes"""
 
     def __init__(self, *args, **kwargs):
         """Constructor"""
-        if kwargs:
-            for key, value in kwargs.items():
-                if key != "__class__":
-                    setattr(self, key, value)
-                if hasattr(self, 'created_at') and type(self.created_at) is str:
-                    self.created_at = datetime.strptime(kwargs['created_at'], time)
-                if hasattr(self, 'updated_at') and type(self.updated_at) is str:
-                    self.updated_at = datetime.strptime(kwargs['created_at'], time)
-        else:
-            
-            uuid_gen = uuid.uuid4()
-            self.id = str(uuid_gen)
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+        uuid_gen = uuid.uuid4()
+        self.id = str(uuid_gen)
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def __str__(self):
         """Returns string representation of an instance"""
@@ -38,7 +29,7 @@ class BaseModel():
         """Returns the attributes of the instance as a dict"""
         var = self.__dict__.copy()
 
-        var['__class__'] = type(self).__class__
+        var['__class__'] = type(self).__name__
         var['created_at'] = self.created_at.isoformat()
         var['updated_at'] = self.updated_at.isoformat()
 
