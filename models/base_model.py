62d2a29bc936bad5d20576cf5f6ff5b540c9fcb8
oscarjsv <gumball_junior@hotmail.com>
2020-06-27 13:36:20 -0500

    addCreate BaseModel from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -3,22 +3,32 @@
 import uuid
 from datetime import datetime
 
+time = "%Y-%m-%dT%H:%M:%S.%f"
 
 class BaseModel():
     """Base class for classes"""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Constructor"""
-        uuid_gen = uuid.uuid4()
-
-        self.id = str(uuid_gen)
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    setattr(self, key, value)
+                if hasattr(self, 'created_at') and type(self.created_at) is str:
+                    self.created_at = datetime.strptime(kwargs['created_at'], time)
+                if hasattr(self, 'updated_at') and type(self.updated_at) is str:
+                    self.updated_at = datetime.strptime(kwargs['created_at'], time)
+        else:
+            
+            uuid_gen = uuid.uuid4()
+            self.id = str(uuid_gen)
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """Returns string representation of an instance"""
         return("[{}] ({}) {}"
-               .format(type(self).__class__, self.id, self.__dict__))
+               .format(type(self).__name__, self.id, self.__dict__))
 
     def save(self):
         """Save an instance and set the updated time"""
